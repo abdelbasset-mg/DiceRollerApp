5379ca0bbdaf082d4c7f1ccfe4394118bb8d8029
abdelbasset-mg <a_meghraoui@estin.dz>
2023-08-12 10:29:44 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
+
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: <Color>[
+            Color(0xFFF26E23),
+            Color(0xFFF24606),
+          ],
+        ),
+      ),
+      child: const Center(
+        child: Text( 'SURVISION',
+            style: TextStyle(
+              color: Color(0xffffffff),
+              fontSize: 28
+              ),
+            ),
+      ),
+    );
+  }
+}
